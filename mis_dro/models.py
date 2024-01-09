5136cc6ec4660e27f6630c6d2423c12ebe4683fb
haritadell <h.dellaporta@gmail.com>
2024-01-09 17:00:47 +0200

    removed unecessary argument d

--- mis_dro/models.py
+++ mis_dro/models.py
@@ -5,13 +5,12 @@ import jax.numpy as jnp
 
 class exponential_model():
 
-    def __init__(self, m, d):
+    def __init__(self, m):
         self.m = m  # number of points sampled from the model at each approximation of the MMD
-        self.d = d  # data dimension
 
     def sample(self,theta, key):
         lamb = jnp.exp(theta) # Re-parametrisation to ensure lambda > 0! 
-        x = jax.random.exponential(key, shape=(self.m,1))/lamb
+        x = jax.random.exponential(key, shape=(self.m,1))/lamb # Exponential with parameter lambda 
 
         return x
     
