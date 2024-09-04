44584d12466358b53f52556ffb6a8244aeaa8c60
haritadell <h.dellaporta@gmail.com>
2024-09-04 22:32:24 +0100

    back to data and Gram matrix as params

--- mis_dro/kdro.py
+++ mis_dro/kdro.py
@@ -40,7 +40,7 @@ class KdroJointEpsBall_Cvxpy():
     '''
     Robustify the joint distribution. 
     '''
-    def __init__(self, dim_theta, loss_call, Xobs, Xcert, K):
+    def __init__(self, dim_theta, loss_call): #, Xobs, Xcert, K
         '''
         #####
         Adjusted from https://github.com/jj-zhu/kdro/blob/main/kdro/kdro.py
@@ -68,9 +68,9 @@ class KdroJointEpsBall_Cvxpy():
 
         self.dim_theta = dim_theta
         self.loss_call = loss_call
-        self.Xobs = Xobs
-        self.Xcert = Xcert
-        self.K = K
+        # self.Xobs = Xobs
+        # self.Xcert = Xcert
+        # self.K = K
 
     def get_problem(self, n_sample, num_certify_samples):
         '''
@@ -79,11 +79,11 @@ class KdroJointEpsBall_Cvxpy():
         # n_sample = num_posterior_samples*num_likelihood_samples
         # sample size for the set of certification points
         n_certify = num_certify_samples
-        # K = cp.Parameter((n_sample+n_certify, n_sample+n_certify), name="K")
-        # K_decomposed = cp.Parameter((n_sample+n_certify, n_sample+n_certify), name="K_decomposed")
+        K = cp.Parameter((n_sample+n_certify, n_sample+n_certify), name="K")
+        K_decomposed = cp.Parameter((n_sample+n_certify, n_sample+n_certify), name="K_decomposed")
         epsilon = cp.Parameter(nonneg=True, name="epsilon")
-        # Xobs = cp.Parameter((n_sample,1), name="Xobs")
-        # Xcert = cp.Parameter((n_certify,1), name="Xcert")
+        Xobs = cp.Parameter((n_sample,1), name="Xobs")
+        Xcert = cp.Parameter((n_certify,1), name="Xcert")
         
         # All variables to be optimized
         theta = cp.Variable(self.dim_theta, name="theta")
@@ -95,27 +95,27 @@ class KdroJointEpsBall_Cvxpy():
         beta = cp.Variable(n_sample+n_certify)
 
         # function values at the kernel_points
-        fvals = self.K @ beta
+        fvals = K @ beta
 
         # List of constraints for cvxpy
         constraints = []
         loss_call = self.loss_call
         # always certify the observations
         for i in range(n_sample):
-            constraints += [loss_call(theta, self.Xobs[i]) 
+            constraints += [loss_call(theta, Xobs[i]) 
             <= f0 + fvals[i] ]
 
         # certify the certifying points
         for i in range(n_certify):
             # wi = self.cert_locs[i]
-            xcert_i = self.Xcert[i]
+            xcert_i = Xcert[i]
             constraints += [loss_call(theta, xcert_i) <= f0 +
             fvals[i+n_sample]]
         
         emp = f0 + cp.sum(fvals[:n_sample]) / n_sample
         # regularization term
         # rkhs_norm = cp.sqrt(cp.quad_form(beta, K + 1e+1*np.eye(K.shape[0])))
-        K_decomposed = np.asarray(mat_decomp_jax(self.K))
+        # K_decomposed = np.asarray(mat_decomp_jax(self.K))
         rkhs_norm = cp.norm(beta.T @ K_decomposed) # pass matdecomp directly
         reg_term = epsilon * rkhs_norm
 
@@ -163,8 +163,8 @@ def main(num_replications,
          n_certify, 
          lengthscale=-1, 
          posterior='mmd',  # mmd bayes
-         algorithm='kdro_exp_mmd',     # bayesian_dro
-         dgp="contaminated_normal",
+         algorithm='kdro_exp_mmd',     # bayesian_dro kdro_exp_mmd
+         dgp="contaminated_exp",
          experiment_dir="./misdro/results_kdro/"):
     
     p = 1  # numbers of unknown parameters
@@ -177,14 +177,17 @@ def main(num_replications,
         "likelihood_time": [],
         "solve_time": [],
     }
+    kdro_class = KdroJointEpsBall_Cvxpy(dim_theta, newsvendor_cost_cvxpy)
+    n_samples = num_posterior_samples*num_likelihood_samples
+    problem = kdro_class.get_problem(n_samples, n_certify)
     #
     # If the number of parameters is small enough, then use Disciplined Parametrized Programming (DPP)
     # to reduce the compilation time in each replication.
     # However, a large number of parameters uses an enormous amout of RAM in the current cvxpy implementation.
-    # ignore_dpp = False
-    # n_parameters = np.sum(np.prod(param.shape) for param in problem.parameters())
-    # if n_parameters >= cp.settings.PARAM_THRESHOLD:
-    #     ignore_dpp = True
+    ignore_dpp = False
+    n_parameters = np.sum(np.prod(param.shape) for param in problem.parameters())
+    if n_parameters >= cp.settings.PARAM_THRESHOLD:
+        ignore_dpp = True
         
     for j in range(num_replications):
         # generate dataset
@@ -234,10 +237,10 @@ def main(num_replications,
             theta_sample = npl_toy.sample
         elif posterior == "bayes":
             # standard Bayesian posterior sample for theta
-            #theta_sample = theta_generation(data, num_posterior_samples, random_state=generator)  # commenting this out for now to try PP
-            prior_params = default_prior_params("normal_known_var")
-            post_parameters = get_posterior_params("normal_known_var", data, prior_params)
-            xi_pp = sample_pp("normal_known_var", post_parameters, num_posterior_samples, generator)
+            theta_sample = theta_generation(data, num_posterior_samples, random_state=generator)  # commenting this out for now to try PP
+            # prior_params = default_prior_params("normal_known_var")
+            # post_parameters = get_posterior_params("normal_known_var", data, prior_params)
+            # xi_pp = sample_pp("normal_known_var", post_parameters, num_posterior_samples, generator)
         times["posterior_time"].append(
             (datetime.now() - posterior_start).total_seconds()
         )
@@ -268,18 +271,18 @@ def main(num_replications,
             # if l == -1: # median heuristic
             l = np.sqrt((1/2)*np.median(distance.cdist(zetai, zetai, 'sqeuclidean')))
             K = k_jax_sym(zetai, zetai, l)
-            # K_decomp = mat_decomp_jax(K)
-            kdro_class = KdroJointEpsBall_Cvxpy(dim_theta, newsvendor_cost_cvxpy, xi, Xcert, K)
-            n_samples = num_posterior_samples*num_likelihood_samples
-            problem = kdro_class.get_problem(n_samples, n_certify)
-            # problem.param_dict["Xobs"].value = xi
-            # problem.param_dict["Xcert"].value = Xcert
-            # problem.param_dict["K"].value = np.asarray(K)
-            # problem.param_dict["K_decomposed"].value = np.asarray(K_decomp)
+            K_decomp = mat_decomp_jax(K)
+            # kdro_class = KdroJointEpsBall_Cvxpy(dim_theta, newsvendor_cost_cvxpy, xi, Xcert, K)
+            # n_samples = num_posterior_samples*num_likelihood_samples
+            # problem = kdro_class.get_problem(n_samples, n_certify)
+            problem.param_dict["Xobs"].value = xi
+            problem.param_dict["Xcert"].value = Xcert
+            problem.param_dict["K"].value = np.asarray(K)
+            problem.param_dict["K_decomposed"].value = np.asarray(K_decomp)
             thetas_list = []
             for eps in EPSILON_SET:
                 problem.param_dict["epsilon"].value = eps
-                problem.solve(cp.MOSEK, verbose=True)  #solver=cp.MOSEK cp.ECOS_BB cp.GUROBI, , ignore_dpp=ignore_dpp
+                problem.solve(cp.MOSEK, verbose=True, ignore_dpp=ignore_dpp)  #solver=cp.MOSEK cp.ECOS_BB cp.GUROBI, , ignore_dpp=ignore_dpp
                 thetas_list.append(problem.var_dict["theta"].value)
             # thetas_list = optimise(newsvendor_cost_cvxpy, 
             #                         xi.reshape((num_posterior_samples*num_likelihood_samples,1)), 
@@ -320,15 +323,15 @@ def main(num_replications,
         "solve_time": times["solve_time"],
         "epsilon": eps
         })
-        df.to_csv(experiment_dir + f"kdro_cont_normal_N100_{eps}.csv", index=False)
+        df.to_csv(experiment_dir + f"kdro_cont_normal_N400_{eps}.csv", index=False)
     
 if __name__ == "__main__":
     num_replications = 100
     num_observations = 20
     num_test_observations = 20
-    num_likelihood_samples = 10
+    num_likelihood_samples = 20
     contamination = 0.1
-    num_posterior_samples = 10
+    num_posterior_samples = 20
     n_certify = 20
             
     main(num_replications, 
