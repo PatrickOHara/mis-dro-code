6065bcbeb6f946bdecd19c35b60c2832c4d342ec
haritadell <h.dellaporta@gmail.com>
2024-09-04 17:08:42 +0100

    contaminated normal

--- mis_dro/dataset.py
+++ mis_dro/dataset.py
@@ -2,7 +2,7 @@
 
 from typing import Optional
 import numpy as np
-from scipy.stats import expon, gamma
+from scipy.stats import expon, gamma, norm
 
 
 def data_generation_outliers(num_observations: int, contamination: float, random_state: Optional[np.random.Generator] = None):
@@ -26,6 +26,28 @@ def data_generation_outliers(num_observations: int, contamination: float, random
     random_state.shuffle(data)  # shuffles the data in-place
     return data
 
+def contaminated_normal(num_observations: int, contamination: float, random_state: Optional[np.random.Generator] = None):
+    """A contaminated Gaussian data-generating process (DGP)
+
+    Args:
+        num_observations: Number of observations from DGP
+        contamination: Ratio of contaminated observations (outliers)
+        random_state: A numpy random generator, if provided
+
+    Notes:
+        Shuffles data to ensure anomalies are not grouped together
+    """
+    if not random_state:
+        random_state = np.random.default_rng()
+    cont_size = int(np.floor(contamination * num_observations))
+    n_real = num_observations - cont_size
+    data = norm.rvs(loc=20, scale=10, size=n_real, random_state=random_state) 
+    outl = norm.rvs(loc=50, scale=10, size=cont_size, random_state=random_state) 
+    data = np.concatenate((data, outl), axis=0)
+    random_state.shuffle(data)  # shuffles the data in-place
+    return data
+    
+
 def data_generation_gamma(num_observations: int, a: float, random_state: Optional[np.random.Generator] = None):
     """A Gamma data-generating process (DGP)
     
@@ -37,7 +59,7 @@ def data_generation_gamma(num_observations: int, a: float, random_state: Optiona
     if not random_state:
         random_state = np.random.default_rng()
         
-    gamma_samples = gamma.rvs(a, size=num_observations)
+    gamma_samples = gamma.rvs(a, loc=50, scale=10 ,size=num_observations)
     
     return gamma_samples
     
