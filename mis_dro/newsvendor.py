8ce708c6aa3799c46b0a69bdd463672a7cc1d170
haritadell <h.dellaporta@gmail.com>
2024-09-18 18:53:08 +0100

    remove cost function in numpy

--- mis_dro/newsvendor.py
+++ mis_dro/newsvendor.py
@@ -16,11 +16,3 @@ def newsvendor_cost_cvxpy(x, xi):
     """
     return HOLDING_COST * cp.maximum(0, x - xi) + BACKORDER_COST * cp.maximum(0, xi - x)
 
-def newsvendor_cost(x, xi):
-    """Evaluate Newsvendor cost function 
-
-    Args:
-        x: Demand decision variable
-        xi: Realised random demand
-    """
-    return HOLDING_COST * np.maximum(0, x - xi) + BACKORDER_COST * np.maximum(0, xi - x)
