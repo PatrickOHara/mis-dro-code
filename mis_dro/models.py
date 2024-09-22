5ddf95bc7bccf5f0f01047b77a7b7c4793c0e3cd
haritadell <h.dellaporta@gmail.com>
2024-09-22 10:12:28 +0100

    add more models for npl

--- mis_dro/models.py
+++ mis_dro/models.py
@@ -2,6 +2,7 @@
 
 import jax
 import jax.numpy as jnp
+from bayesian_dro.Bayesian_DRO_continuous import DGP_STD_TRUNCATED_NORMAL
 
 
 class ExponentialModel:
@@ -13,8 +14,16 @@ class ExponentialModel:
         x = (
             jax.random.exponential(key, shape=(self.m, 1)) / lamb
         )  # Exponential with parameter lambda
-
         return x
+    
+    def init_params(self, data):
+        # This function return the initialisation parameters for the minimisation of the mmd
+        return jnp.log((1/jnp.mean(data)))*jnp.ones(1) # Initialisation of unknown parameter, here I inistialise at MLE
+    
+    def parametrise(self, theta):
+        return jnp.exp(theta)  # rate parameter of expoenential model is re-parametrised to ensure positivity
+        
+        
     
 class univariate_GaussianModel:
     def __init__(self, m):
@@ -22,12 +31,35 @@ class univariate_GaussianModel:
     
     def sample(self, theta, key):
         mu = theta[0]
-        std = theta[1]
+        std = jnp.exp(theta[1]) # make sure standard deviation is positive!
         x = (
             mu + std*jax.random.normal(key, shape=(self.m,1))
         )
         
         return x
+    
+    def init_params(self, data):
+        return jnp.array([jnp.mean(data), jnp.log(jnp.std(data))]).reshape((2,))
+    
+    def parametrise(self, theta): 
+        theta = theta.at[1].set(jnp.exp(theta[1]))  # scale parameter is reparametrised to ensure postivity - now parametrise back
+        return theta 
+    
+class univariate_GaussianModel_known_variance:
+    def __init__(self, m):
+        self.m = m
+    
+    def sample(self, theta, key):
+        mu = theta[0]
+        std = DGP_STD_TRUNCATED_NORMAL
+        x = (
+            mu + std*jax.random.normal(key, shape=(self.m,1))
+        )
+        
+        return x
+    
+    def init_params(self, data):
+        return jnp.mean(data).reshape((1,))
 
 class multivariate_GaussianModel:
     def __init__(self, m, d):
@@ -35,10 +67,17 @@ class multivariate_GaussianModel:
         self.d = d
     
     def sample(self, theta, key):
-        mu, sigma = theta
+        mu = theta
+        sigma = DGP_STD_TRUNCATED_NORMAL
         x = (
-            jax.random.multivariate_normal(key, mean = mu, cov = sigma, shape=(self.m,self.d))
+            jax.random.multivariate_normal(key, mean = mu, cov = sigma*jnp.eye(self.d), shape=(self.m,self.d))
         )
         
         return x
+    
+    def init_params(self, data):
+        return  jnp.mean(data, axis=0).reshape((self.d,))
+    
+
+        
     
