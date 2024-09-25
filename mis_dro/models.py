82148f211499eeb0fde2fe070d8c1de914d346a4
haritadell <h.dellaporta@gmail.com>
2024-09-25 12:06:17 +0100

    parametrisation in regression

--- mis_dro/models.py
+++ mis_dro/models.py
@@ -92,29 +92,30 @@ class regression_GaussianModel:
     def sample(self, theta, key):
         a = theta[0]
         b = theta[1]
-        std = std = jnp.exp(theta[2]) # make sure standard deviation is positive!
+        var = theta[2] # make sure standard deviation is positive!
         mu = a - b*self.price
         demand = (
-            jax.random.multivariate_normal(key, mean=mu, cov=(std**2)*jnp.eye(self.m), shape=(self.m,1))
+            jax.random.multivariate_normal(key, mean=mu, cov=var*jnp.eye(self.m), shape=(self.m,1))
         )
         return demand
     
-    def init_params(self, data):
+    def init_params(self, data, covariates):
         # For regression parameters initialise with the OLS estimator 
         # For the std initialise with the sample std
         init_theta = jnp.zeros(3)
-        std_init = jnp.log(jnp.std(data))
         # regression terms
-        X = jnp.column_stack((jnp.ones_like(data[:,0]), -data[:,0]))
-        coeffs, _, _, _ = jnp.linalg.lstsq(X, data[:,1], rcond=None)
+        X = jnp.column_stack((jnp.ones_like(covariates), -covariates))
+        coeffs, residuals, _, _ = jnp.linalg.lstsq(X, data, rcond=None)
+        SSR = residuals[0]
+        var_init = SSR / (len(data) - 2)
         a_init, b_init = coeffs
-        init_theta = init_theta.at[0].set(a_init)
-        init_theta = init_theta.at[1].set(b_init)
-        init_theta = init_theta.at[2].set(std_init)
+        init_theta = init_theta.at[0].set(a_init[0])
+        init_theta = init_theta.at[1].set(b_init[0])
+        init_theta = init_theta.at[2].set(var_init)
         return init_theta
     
     def parametrise(self, theta):
-        theta = theta.at[2].set(jnp.exp(theta[2]))
+        theta = theta.at[2].set(jnp.sqrt(theta[2])) # return the std
         return theta
         
     
