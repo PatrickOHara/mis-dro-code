04f736cf0a87989d52ff1815fe8cfbb2d0391d7a
PatrickOHara <patrick.h.o-hara@warwick.ac.uk>
2024-10-03 18:12:40 +0100

    Change contaminated exponential DGP to 20

--- mis_dro/dataset.py
+++ mis_dro/dataset.py
@@ -75,7 +75,7 @@ def data_generation_outliers(
         random_state = np.random.default_rng()
     cont_size = int(np.floor(contamination * num_observations))
     n_real = num_observations - cont_size
-    data = expon.rvs(scale=10, size=n_real, random_state=random_state)
+    data = expon.rvs(scale=20, size=n_real, random_state=random_state)
     # outl = expon.rvs(scale=70, size=cont_size, random_state=random_state)
     outl = norm.rvs(loc=100, scale=0.5, size=cont_size, random_state=random_state) 
     data = np.concatenate((data, outl), axis=0)
