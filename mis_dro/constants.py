15c614743800c405aa86f64a2673ec7f9eeff007
PatrickOHara <patrick.h.o-hara@warwick.ac.uk>
2024-10-08 11:00:00 +0100

    Something is broken in normal

--- mis_dro/constants.py
+++ mis_dro/constants.py
@@ -11,7 +11,7 @@ NUM_CERTIFY = 200 # num certufying points for discretisation of KDRO problem con
 MAX_PARAMS_OOM = 1000   # if the number of params of a cvxpy exceeds this number, we might go out-of-memory
 IN_SAMPLE_TIME_WINDOW = 52
 OUT_OF_SAMPLE_TIME_WINDOW = 12  # number of weeks in out-of-sample period
-BAS_TOTAL_MODEL_SAMPLES = [25, 100, 900, 2500]
+BAS_TOTAL_MODEL_SAMPLES = [25, 100, 900]
 
 BAS_DRO_EPSILON_SET = [
     0.001,
