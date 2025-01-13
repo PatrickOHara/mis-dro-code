195baf85dfa55706068a7eeae923e1f23e0b84ae
Patrick O'Hara <patrick.h.o-hara@warwick.ac.uk>
2025-01-13 10:47:25 +0000

    Update normal gamma notebook

--- mis_dro/plot.py
+++ mis_dro/plot.py
@@ -25,7 +25,8 @@ class AlgorithmColor(StrEnum):
     """Colors of algorithm lines"""
     dro_bas_mmd = "#008080"   #"#f781bf"
     kl_bdro = "#FF800E"
-    kl_dro_bas = "#8A2BE2" #"#006BA4"
+    # kl_dro_bas = "#8A2BE2" 
+    kl_dro_bas = "#006BA4"
     empirical_mmd = "#999999"
     kl_pp = "red"
 
