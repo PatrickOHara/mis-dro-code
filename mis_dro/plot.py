7a3bce0eb6fe56059f18824b7a5bbb523cf112b1
PatrickOHara <patrick.h.o-hara@warwick.ac.uk>
2025-01-30 16:53:12 +0000

    Updated plotting code

--- mis_dro/plot.py
+++ mis_dro/plot.py
@@ -34,13 +34,12 @@ CB_color_cycle = [
 
 class AlgorithmColor(StrEnum):
     """Colors of algorithm lines"""
-    dro_bas_mmd = "#008080"   #"#f781bf"
-    kl_bdro = "#FF800E"
-    # kl_dro_bas = "#8A2BE2" 
-    kl_dro_bas = "#006BA4"
+    dro_bas_mmd = "#377eb8"   #"#f781bf"
+    kl_bdro = "#ff7f00"
+    kl_dro_bas = "#984ea3"
     kl_empirical = "#999999"
     empirical_mmd = "#999999"
-    kl_pp = "#984ea3"
+    kl_pp = "#4daf4a"
 
 
 
