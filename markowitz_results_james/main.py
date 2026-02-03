44009b31e30c8c4f2b11b0c39c6a21fe53792297
James Graca-Jones <jamesgracajones@gmail.com>
2026-02-03 16:31:55 +0000

    Move risk-free rates TODO to correct location and add a very important new TODO regarding getting validation data for Markowitz in rolling temporal validation

--- markowitz_results_james/main.py
+++ markowitz_results_james/main.py
@@ -222,9 +222,10 @@ def get_data_for_rolling_validation(markowitz_training_df, test_df, num_folds, d
 
         rolling_validation_training_df = markowitz_training_df[fold_index: validation_start_index_for_fold]
 
-        # TODO: note that 51 is specific to the risk free returns file in use (because it has 51 weekly risk-free rates up to and including the first rebalance date)
+        # TODO: IMPORTANT: fix how the slice has been done here, for now it is wrong because the way it is done assumes dro_windows[window_index][0] has the same size as markowitz_training_df, I believe.
         rolling_validation_validation_df = dro_windows[window_index][0][validation_start_index_for_fold: validation_start_index_for_fold + rolling_validation_validation_df_size]
 
+        # TODO: note that 51 is specific to the risk free returns file in use (because it has 51 weekly risk-free rates up to and including the first rebalance date)
         risk_free_rates_for_validation = choose_risk_free_rates_for_rolling_validation(risk_free_rates, 51, 13, len(rolling_validation_validation_df), window_index, num_folds, fold_index)
 
         rolling_validation_folds.append({
